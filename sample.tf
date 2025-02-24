82543b9897bb4adb8483fc8f3be34ab38b17e50b
meghasyam1997 <meghasyam01@gmail.com>
2025-02-24 12:43:29 +0530

    frontend

--- sample.tf
+++ sample.tf
@@ -1,8 +1,19 @@
-resource "aws_instance" "test" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.micro"
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
+}
+
+data "aws_ami" "ami" {
+  owners      = ["973714476881"]
+  name_regex  = "RHEL-9-DevOps-Practice"
+  most_recent = true
+
+  /*filter {
+    name   = "RHEL-9-DevOps-Practice"
+    values = ["ami-09c813fb71547fc4f"]
+  }*/
 }
